547cbdc63a5f7aa10080a2f5230376e38430cead
xnhyacinth <huanxuanliao@gmail.com>
2026-03-31 00:17:45 +0800

    style: redesign IAG, TAGI, and ResAdapt project pages with modern academic layout

--- projects/ResAdapt/static/js/index.js
+++ projects/ResAdapt/static/js/index.js
@@ -1,21 +1,21 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
 
-$(document).ready(function() {
-    // Check for click events on the navbar burger icon
-
+$(document).ready(function () {
     var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 5000,
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        loop: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
     }
 
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
-	
-    bulmaSlider.attach();
+    if (document.querySelectorAll('.carousel').length) {
+        bulmaCarousel.attach('.carousel', options);
+    }
 
+    if (typeof bulmaSlider !== 'undefined' && bulmaSlider.attach) {
+        bulmaSlider.attach();
+    }
 })
